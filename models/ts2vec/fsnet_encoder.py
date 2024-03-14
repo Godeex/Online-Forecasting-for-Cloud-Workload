230a4289ad2efd061ca3fb04ce2cc71efbe8fef5
Godeex <56467260+Godeex@users.noreply.github.com>
2024-03-14 10:58:03 +0800

    initialize

---
+++ models/ts2vec/fsnet_encoder.py
@@ -0,0 +1,127 @@
+import math
+from typing import List, Optional
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+import numpy as np
+from einops import rearrange, reduce, repeat
+
+from .fsnet_ import DilatedConvEncoder
+
+
+def generate_continuous_mask(B, T, n=5, l=0.1):
+    res = torch.full((B, T), True, dtype=torch.bool)
+    if isinstance(n, float):
+        n = int(n * T)
+    n = max(min(n, T // 2), 1)
+    
+    if isinstance(l, float):
+        l = int(l * T)
+    l = max(l, 1)
+    
+    for i in range(B):
+        for _ in range(n):
+            t = np.random.randint(T-l+1)
+            res[i, t:t+l] = False
+    return res
+
+
+def generate_binomial_mask(B, T, p=0.5):
+    return torch.from_numpy(np.random.binomial(1, p, size=(B, T))).to(torch.bool)
+
+
+class TSEncoder(nn.Module):
+    def __init__(self, input_dims, output_dims, hidden_dims=64, depth=10, mask_mode='binomial', gamma=0.9, groups=1):
+        super().__init__()
+        self.input_dims = input_dims
+        self.output_dims = output_dims
+        self.hidden_dims = hidden_dims
+        self.mask_mode = mask_mode
+        # self.input_fc = nn.Linear(input_dims, hidden_dims)
+        self.input_fc = nn.Sequential(nn.Linear(input_dims, hidden_dims), nn.ReLU())
+        self.feature_extractor = DilatedConvEncoder(
+            hidden_dims,
+            [hidden_dims] * depth + [output_dims],
+            kernel_size=3, gamma=gamma, groups=groups
+        )
+        self.repr_dropout = nn.Dropout(p=0.1)
+
+        # [64] * 10 + [320] = [64, 64, 64, 64, 64, 64, 64, 64, 64 ,64, 320] = 11 items
+        # for i in range(len(...)) -> 0, 1, ..., 10
+    
+    def ctrl_params(self):
+        return self.feature_extractor.ctrl_params()
+
+    def forward(self, x, mask=None):  # x: B x T x input_dims
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch
+
+        # generate & apply mask
+        if mask is None:
+            if self.training:
+                mask = self.mask_mode
+            else:
+                mask = 'all_true'
+
+        if mask == 'binomial':
+            mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'continuous':
+            mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'all_true':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+        elif mask == 'all_false':
+            mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
+        elif mask == 'mask_last':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+            mask[:, -1] = False
+
+        mask &= nan_mask
+        x[~mask] = 0
+
+        # conv encoder
+        x = x.transpose(1, 2)  # B x Ch x T
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B x T x Co
+        # print("x_v_3:", x.size())
+
+        return x
+
+    def forward_time(self, x, mask=None):  # x: B x T x input_dims
+        x = x.transpose(1, 2)
+        nan_mask = ~x.isnan().any(axis=-1)
+        x[~nan_mask] = 0
+        x = self.input_fc(x)  # B x T x Ch
+        
+        # generate & apply mask
+        if mask is None:
+            if self.training:
+                mask = self.mask_mode
+            else:
+                mask = 'all_true'
+        
+        if mask == 'binomial':
+            mask = generate_binomial_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'continuous':
+            mask = generate_continuous_mask(x.size(0), x.size(1)).to(x.device)
+        elif mask == 'all_true':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+        elif mask == 'all_false':
+            mask = x.new_full((x.size(0), x.size(1)), False, dtype=torch.bool)
+        elif mask == 'mask_last':
+            mask = x.new_full((x.size(0), x.size(1)), True, dtype=torch.bool)
+            mask[:, -1] = False
+        
+        mask &= nan_mask
+        x[~mask] = 0
+        
+        # conv encoder
+        x = x.transpose(1, 2)  # B x Ch x T
+        x = self.repr_dropout(self.feature_extractor(x))  # B x Co x T
+        x = x.transpose(1, 2)  # B x T x Co
+        # print("x_time_3:", x.size())
+        return x
+
+
